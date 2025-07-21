62db2118e0938abb6b1c34c2c88e8e6134bad5a5
mensahAmpomah <gideonmensah201@gmail.com>
2025-07-20 21:02:52 -0700

    django

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,13 +1,1 @@
-from relationship_app.models import Library
-
-library = Library.objects.get(name="Accra Library")
-books = library.books.all()
-for book in books:
-    print(book.title)
-
-from relationship_app.models import Book
-
-book1 = Book.objects.get(title="Django Basics")
-book2 = Book.objects.get(title="Advanced Django")
-
-library.books.set([book1, book2])
+library = Library.objects.get(name=library_name)
