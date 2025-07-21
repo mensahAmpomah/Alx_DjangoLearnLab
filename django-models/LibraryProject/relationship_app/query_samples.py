51b8ea124ca8cd8b8bf55ae0db7279882d3e9bc7
mensahAmpomah <gideonmensah201@gmail.com>
2025-07-20 20:57:15 -0700

    django

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -8,13 +8,14 @@ book1 = Book.objects.create(title="My Word",author=Author)                      
 book2 = Book.objects.create(title="Psalms for you",author=Author)
 
 library = Library.objects.create(name="Ketia")
+
 library.books.set([book1,book2])
 
 librarian = Librarian.objects.create(name="Yaw Manu",library=library)
-
+library = Library.objects.get(name="Accra Library")
 books = Book.object.filter(author__name= "Kofi Mante")
 for book in library.books.all():
     print(book.title)
 
-library = Library.objects.get(name="Accra Library")
+
 print(library.librarian.name)
