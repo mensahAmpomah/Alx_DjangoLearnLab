185d20f4a5d429af72fd696b4fb337aa1cbe4385
mensahAmpomah <gideonmensah201@gmail.com>
2025-07-27 23:16:17 -0700

    Permission

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import permission_required, login_required
-from .models import Article
+from .models import Article, Book
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def article_list(request):
