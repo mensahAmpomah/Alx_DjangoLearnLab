17836a7d4cf560b1a481a533177ad56ed937d980
mensahAmpomah <gideonmensah201@gmail.com>
2025-07-27 23:42:50 -0700

    Security Practices

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Book
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'description', 'published_date']
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        # Sanitize input - prevent malicious input or check rules
+        if len(title) < 3:
+            raise forms.ValidationError("Title must be at least 3 characters long.")
+        return title
