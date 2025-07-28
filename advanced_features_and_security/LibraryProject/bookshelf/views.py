ba61360ee4146662bc3b71ef655f6155048660c7
mensahAmpomah <gideonmensah201@gmail.com>
2025-07-27 23:07:50 -0700

    Permissions

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import permission_required, login_required
+from .models import Article
 
-# Create your views here.
+@permission_required('bookshelf.can_view', raise_exception=True)
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'bookshelf/article_list.html', {'articles': articles})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def article_create(request):
+    # Logic for creating article
+    return render(request, 'bookshelf/article_create.html')
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def article_edit(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    # Logic for editing article
+    return render(request, 'bookshelf/article_edit.html', {'article': article})
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def article_delete(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    # Logic for deleting article
+    return render(request, 'bookshelf/article_delete.html', {'article': article})
