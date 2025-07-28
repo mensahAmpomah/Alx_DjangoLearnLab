0a15881962e24fec5009af3eda00cba14f1780bf
mensahAmpomah <gideonmensah201@gmail.com>
2025-07-27 22:47:21 -0700

    Advanced features and Security

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshelf',
-    'custom'
+    
 ]
 
 MIDDLEWARE = [
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-AUTH_USER_MODEL = 'custom.CustomUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Book
-
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin 
 # Register your models here.
 
 @admin.register(Book)
@@ -8,3 +9,12 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title','author','publication_year')
     list_filter = ('author','publication_year')
     search_fields = ('title','author')
+
+
+
+# Register your models here.
+
+class CustomerUserAdmin(UserAdmin):
+    pass
+
+admin.site.register(CustomUser)
