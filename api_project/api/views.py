60d56fec5efa043d2664129029b7666984f3e0b9
mensahAmpomah <gideonmensah201@gmail.com>
2025-08-03 22:08:59 -0700

    Fixing the ListAPIView

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -5,6 +5,6 @@ from .models import Book
 
 # Create your views here.
 
-class BookList(generics.ListCreateAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
