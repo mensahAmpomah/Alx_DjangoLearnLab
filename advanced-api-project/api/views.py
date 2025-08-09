ac958234453b82f4ecf6f3295e1b997be4dcb1f8
mensahAmpomah <gideonmensah201@gmail.com>
2025-08-09 08:17:35 -0700

    Added the IsAuthenticated and IsAuthenticatedReadOnly

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,38 +1,39 @@
 from django.shortcuts import render
 from .models import Book, Author
 from .serializers import BookSerializer
-from rest_framework import generics, permissions
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly
 
 
 # Creating of Book Here.
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 # list all books
 class BookListView(generics.ListAPIView):
     queryset = Book.Objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 # Retrieve single book by ID
 class BookDetailView(generics.RetrieveAPIView):
     queryset = Book.Objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 # Update a Book
 class BookUpdateView(generics.UpdateAPIView):
     queryset = Book.Objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 # Delete a Book
 
 class BookDeleteView(generics.UpdateAPIView):
     queryset = Book.Objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 
