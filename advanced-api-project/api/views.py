a02cd250daabc9b398a0f0509f09ac221a51c283
mensahAmpomah <gideonmensah201@gmail.com>
2025-08-09 08:05:32 -0700

    Building of Custom Views and Generic Views in DRF

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from .models import Book, Author
+from .serializers import BookSerializer
+from rest_framework import generics, permissions
+
 
 # Create your views here.
+# list all books
+class BookListView(generics.ListAPIView):
+    queryset = Book.Objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+# Retrieve single book by ID
+class BookDetailView(generics.RetrieveAPIView):
+    queryset = Book.Objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.AllowAny]
+
+# Update a Book
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.Objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+# Delete a Book
+
+class BookDeleteView(generics.UpdateAPIView):
+    queryset = Book.Objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
