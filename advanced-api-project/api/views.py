abc8758521402271f29f0bb4fd337cce832821e3
mensahAmpomah <gideonmensah201@gmail.com>
2025-08-09 08:24:40 -0700

    Changed the directions of the IsAuthenticated and IsAuthenticatedOrReadOnly positions

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Book, Author
 from .serializers import BookSerializer
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated,IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 
 # Creating of Book Here.
