8188c8f95d225e61c0badbdac425856d64f6da94
mensahAmpomah <gideonmensah201@gmail.com>
2025-08-16 11:31:48 -0700

    work on the post/new

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
-    path('posts/', views.PostListView.as_view(), name='posts'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/', views.PostListView.as_view(), name='posts'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
