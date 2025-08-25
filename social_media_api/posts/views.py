3cd80813ba0b5cc0d529e22faf8e17ce765c90c3
mensahAmpomah <gideonmensah201@gmail.com>
2025-08-24 21:14:35 -0700

    added the  objects.all() in the views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,35 +1,24 @@
-from django.shortcuts import render
 from rest_framework import viewsets, permissions, filters
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from .permissions import IsOwnerOrReadOnly
 
+# Post ViewSet
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.select_related('author').all()
+    queryset = Post.objects.all()  
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-    search_fields = ['title', 'content', 'author__username']
-    ordering_fields = ['created_at', 'updated_at', 'title']
-    ordering = ['-created_at']
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content'] 
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.select_related('author', 'post').all()
-    serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    filter_backends = [filters.OrderingFilter]
-    ordering_fields = ['created_at', 'updated_at']
-    ordering = ['created_at']
 
-    def get_queryset(self):
-        qs = super().get_queryset()
-        post_id = self.request.query_params.get('post')
-        if post_id:
-            qs = qs.filter(post_id=post_id)
-        return qs
+# Comment ViewSet
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()  
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
